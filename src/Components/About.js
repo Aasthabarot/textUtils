c5f1cf74feb07f0e293e6a85a8b49ac355717178
Aasthabarot <barotaastha38@gmail.com>
2024-07-11 09:07:04 +0530

    add

--- src/Components/About.js
+++ src/Components/About.js
@@ -33,6 +33,7 @@ export default function About() {
 
      }
    
+     
   return (
     
       <>
--- src/Components/TextForm.js
+++ src/Components/TextForm.js
@@ -27,6 +27,18 @@ export default function TextForm(props) {
             //  console.log("on change");
              setText(event.target.value)
         }
+      
+        const handleCopy = ()=>
+          {
+               let text = document.getElementById("myBox");
+               text.select();
+               navigator.clipboard.writeText(text.value);
+          }
+
+          const handleExtraSpaces = () => {
+            let newtext = text.split(/\s+/);
+            setText(newtext.join(" ").trim());
+        }
         
 
 
@@ -40,6 +52,8 @@ export default function TextForm(props) {
 </div>
 <button className='btn btn-primary mx-3' onClick={handleUpClick}>Convert to Uppercase</button>
 <button className='btn btn-primary mx-3' onClick={handleLoClick}>Convert to Lowercase</button>
+<button className='btn btn-primary mx-3' onClick={handleExtraSpaces}>Remove Extra Spaces</button>
+<button className='btn btn-primary mx-3' onClick={handleCopy}>Copy Text</button>
 <button className='btn btn-primary mx-3' onClick={handleClearClick}>Clear Text</button>
 </div>
 <div className="container my-3">
